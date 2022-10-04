9c7e890cf8a8b1002b5f583ec9c2bef45f1d2724
Ilia Mediankin <i.mediankin@gmail.com>
2022-10-05 00:43:32 +0300

    Initial commit

---
+++ Lesson 1-7/hello_world_numbers.rb
@@ -0,0 +1,12 @@
+print "Enter X: "
+x = gets.chomp
+
+print "Enter Y: "
+y = gets.to_i
+
+print "Enter Z: "
+z = gets.to_f
+
+puts
+print "x = " + x + "  y = " + y.to_s + "  z = " + z.to_s
+gets
---
+++ Lesson 1-7/name.length.rb
@@ -0,0 +1,12 @@
+puts
+print "Введите ваше имя: "
+name = gets.strip.capitalize
+
+# 10.downto(3) { |x| puts x } 
+# puts "Добрый день, #{name.capitalize}"
+# puts "В Вашем имени #{name .length} букв"
+
+# puts "Вас зовут:\t #{name}"
+# puts "Букв в имени:\t #{name.length}"
+
+puts "Вас зовут:\t #{name} \nБукв в имени:\t #{name.length}"
