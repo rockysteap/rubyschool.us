403c7a0d75cebb2cd03c943256cbff0d3ae5d42d
Ilia Mediankin <i.mediankin@gmail.com>
2022-10-05 00:53:17 +0300

    Initial commit delete

--- Lesson 1-7/hello_world.rb
+++
@@ -1,7 +0,0 @@
-print "Enter your name: "
-myname = gets.chomp
-
-print "Enter your age: "
-age = gets.chomp
-
-puts "Hello " + myname + " your age is " + age + " !!!"
--- Lesson 1-7/ohm's_law.rb
+++
@@ -1,6 +0,0 @@
-print "Enter voltage: "
-U = gets.chomp
-print "Enter resistance: "
-R = gets.chomp
-I = U.to_f / R.to_f
-print "Result: " + I.to_s + " A"
